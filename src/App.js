130b05fd1bcc68974e5ad1b184a57e6e7aa90065
AGUSTIN FULCO <47069276@ort>
2023-06-15 09:59:11 -0300

    terminado sin opcionales

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import './App.css';
 import axios from 'axios'
-import { useEffect } from 'react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 function App() {
   const [count, setCount] = useState(0)
   const [bandera, setBandera] = useState({})
+
   const puntaje = evento => {
-    alert(evento.target.bandera.value)
-    setCount(count + 10) 
+    evento.preventDefault();  
+    if (evento.target.bandera.value === bandera.name) {
+      setCount(count + 10) 
+    }
+    else {
+      setCount(count - 1)
+    }
+
   }
   useEffect(() => {
-    /*axios.post('https://countriesnow.space/api/v0.1/countries/flag/unicode', {
-      country: 'N'
-    })*/
-  })
+    axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
+      .then(function (response) {
+        const banderaRandom = response.data.data[Math.floor(Math.random()*response.data.data.length)]
+        setBandera( 
+          banderaRandom
+        )
+        console.log(bandera)
+      })
+  }, [count])
   return (
-    <div className="App">
-      <form onSubmit={e => puntaje(e)}>
-        <input type="text" name='bandera'></input>
-        <button>Enviar</button>
-      </form>
-       <h1>Puntaje: {count}</h1>
-    </div>
+    <body>
+      <div className="container">
+        <div className='center'>
+          <h1>Puntaje: {count}</h1>
+          <img src={bandera.flag} alt="no hay bandera"></img>
+          <form onSubmit={e => puntaje(e)}>
+            <input type="text" name='bandera'></input>
+            <button>Enviar</button>
+          </form>
+        </div>
+      </div>
+    </body>
   );
 }
 
