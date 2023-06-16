a67e470f6465cb6f10c5dc5ba8a9b79f2f801e88
TIAGO EZEQUIEL GLUSMAN <47201568@ort>
2023-06-16 11:45:40 -0300

    timer medio raro

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [count, setCount] = useState(0)
   const [bandera, setBandera] = useState({})
-
+  const [timer, setTimer] = useState()
   const puntaje = evento => {
     evento.preventDefault();  
-    if (evento.target.bandera.value === bandera.name) {
+    if (evento.target.bandera.value.toLowerCase() === bandera.name.toLowerCase()) {
       setCount(count + 10) 
     }
     else {
@@ -15,16 +15,20 @@ function App() {
     }
 
   }
+  
   useEffect(() => {
-    axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
+      axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
       .then(function (response) {
         const banderaRandom = response.data.data[Math.floor(Math.random()*response.data.data.length)]
         setBandera( 
           banderaRandom
         )
-        console.log(bandera)
+        setTimer(15)
       })
   }, [count])
+  setInterval(() => {
+    setTimer(timer - 1)
+  }, 1000)
   return (
     <body>
       <div className="container">
@@ -34,6 +38,7 @@ function App() {
           <form onSubmit={e => puntaje(e)}>
             <input type="text" name='bandera'></input>
             <button>Enviar</button>
+            {timer}
           </form>
         </div>
       </div>
