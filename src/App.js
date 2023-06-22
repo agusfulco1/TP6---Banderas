df49900246dc901ebbe825bcc574b031d1ee967e
TIAGO EZEQUIEL GLUSMAN <47201568@ort>
2023-06-22 10:37:01 -0300

    timer terminado y un poco de diseño

--- src/App.js
+++ src/App.js
@@ -1,45 +1,67 @@
 import './App.css';
 import axios from 'axios'
 import React, { useState, useEffect } from 'react';
+
 function App() {
   const [count, setCount] = useState(0)
   const [bandera, setBandera] = useState({})
-  const [timer, setTimer] = useState()
+  const [timer, setTimer] = useState(15);
+  const [ref, setRef] = useState();
+
   const puntaje = evento => {
-    evento.preventDefault();  
+    evento.preventDefault();
     if (evento.target.bandera.value.toLowerCase() === bandera.name.toLowerCase()) {
-      setCount(count + 10) 
+      setCount(count + 10)
     }
     else {
       setCount(count - 1)
     }
-
+    clearTimeout(ref)
+    evento.target.bandera.value = ""
   }
-  
+
   useEffect(() => {
-      axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
+    axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
       .then(function (response) {
-        const banderaRandom = response.data.data[Math.floor(Math.random()*response.data.data.length)]
-        setBandera( 
-          banderaRandom
-        )
+        const banderaRandom = response.data.data[Math.floor(Math.random() * response.data.data.length)]
+        setBandera(banderaRandom);
         setTimer(15)
       })
   }, [count])
-  setInterval(() => {
-    setTimer(timer - 1)
-  }, 1000)
+
+  useEffect(() => {
+    setRef(setTimeout(() => {
+      if (timer > 0) {
+        setTimer(t => t - 1);
+      }
+      else {
+        clearTimeout(ref)
+        setCount(count - 1)
+      }
+    }, 1000));
+  }, [timer])
+
   return (
     <body>
-      <div className="container">
-        <div className='center'>
-          <h1>Puntaje: {count}</h1>
-          <img src={bandera.flag} alt="no hay bandera"></img>
-          <form onSubmit={e => puntaje(e)}>
-            <input type="text" name='bandera'></input>
-            <button>Enviar</button>
-            {timer}
+      <div className="container-fluid">
+        <div className='row d-flex justify-content-start'>
+          <div className='col-3 borde'>
+            <div className='container'>
+              <h2 className='count'>Puntaje: {count}</h2>
+            </div>  
+          </div>
+          <div className='col-6'>
+            <img src={bandera.flag} alt="no hay bandera"></img>
+            <form onSubmit={e => puntaje(e)}>
+              <input className="input"type="text" name='bandera'></input>
+              <button>Enviar</button>
           </form>
+          </div>
+          <div className='col-3 borde'>
+           <div className='container'>
+                <h2 className='timer'>Te quedan: {timer} segundos</h2>
+            </div>
+          </div>
         </div>
       </div>
     </body>
